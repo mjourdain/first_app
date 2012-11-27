482f45a79e8c5eb48d5aaa0397d7b7af5f7248d7
jourdain.marine@gmail.com <telect@telect-laptop.(none)>
2012-11-27 20:42:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'aa213e93231717805eae9ec785e6ed9ec24a427f30a5c96426640d62ff7ae3585852715258f77b3cc1629fdb83a306b3fffebe92da2ec44a5dc73e3f5deec9f6'
